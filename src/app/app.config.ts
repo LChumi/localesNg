ef17478eb42627cda896b4eced2cbd21984e0da5
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-06-20 16:19:15 -0500

    feat (Se agrega toastr)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideToastr({ timeOut: 900, preventDuplicates: true}),
     provideRouter(routes,withViewTransitions()),
     provideHttpClient(withFetch(), withInterceptors([]))
   ]
