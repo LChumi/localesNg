9917598109b711d2328bc526d3b25ab0a3de1c41
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-10-01 08:53:50 -0500

    fix(change toi environments )

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrlBase: `http://192.168.18.209:8080/`,//`http://localhost:8080/`,
+  apiUrlBase: `http://192.168.191.209:8080/`,//`http://localhost:8080/`,
   production: false
 }
